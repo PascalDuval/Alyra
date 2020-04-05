83a7774a59ce51eb676113bd14ffcf918ae9bfae
Pascal Duval <PascalDuval@users.noreply.github.com>
2020-04-05 10:57:44 +0200

    Add files via upload
    modèle de pow naîve : longueur du mot <= 8 - Debut du mot : [nx ex: 7c] 
    Entrez une longueur [8 maxi] : 8
    Entrez un début de chaîne Hexa [2 caractères maxi] : e3
             53 function calls in 8.116 seconds
    
       Ordered by: standard name
    
       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
            1    0.000    0.000    0.000    0.000 <string>:1(<module>)
            1    0.000    0.000    0.000    0.000 __future__.py:48(<module>)
            1    0.000    0.000    0.000    0.000 __future__.py:74(_Feature)
            7    0.000    0.000    0.000    0.000 __future__.py:75(__init__)
            1    0.000    0.000    8.116    8.116 exo1-4-12-Alyra.py:6(<module>)
            6    0.000    0.000    0.000    0.000 hashlib.py:100(__get_openssl_constructor)
            1    0.003    0.003    0.003    0.003 hashlib.py:56(<module>)
            1    0.000    0.000    0.002    0.002 random.py:100(seed)
            1    0.001    0.001    0.005    0.005 random.py:40(<module>)
            1    0.000    0.000    0.000    0.000 random.py:655(WichmannHill)
            1    0.000    0.000    0.000    0.000 random.py:72(Random)
            1    0.000    0.000    0.000    0.000 random.py:805(SystemRandom)
            1    0.000    0.000    0.002    0.002 random.py:91(__init__)
            1    0.000    0.000    0.000    0.000 {_hashlib.openssl_md5}
            1    0.000    0.000    0.000    0.000 {_hashlib.openssl_sha1}
            1    0.000    0.000    0.000    0.000 {_hashlib.openssl_sha224}
            1    0.000    0.000    0.000    0.000 {_hashlib.openssl_sha256}
            1    0.000    0.000    0.000    0.000 {_hashlib.openssl_sha384}
            1    0.000    0.000    0.000    0.000 {_hashlib.openssl_sha512}
            1    0.000    0.000    0.000    0.000 {binascii.hexlify}
            1    0.001    0.001    0.001    0.001 {function seed at 0x7f2ed5e1dde8}
            6    0.000    0.000    0.000    0.000 {getattr}
            6    0.000    0.000    0.000    0.000 {globals}
            2    8.110    4.055    8.110    4.055 {input}
            1    0.000    0.000    0.000    0.000 {math.exp}
            2    0.000    0.000    0.000    0.000 {math.log}
            1    0.000    0.000    0.000    0.000 {math.sqrt}
            1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
            1    0.000    0.000    0.000    0.000 {method 'union' of 'set' objects}
            1    0.000    0.000    0.000    0.000 {posix.urandom}

---
+++ exo1-4-12-Alyra.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+#Écrire le même programme en utilisant une fonction de hachage connue (comme SHA-256 par exemple)
+#Mesurer les performances en fonction de la fonction de hachage utilisée et de la longueur de la chaîne recherchée.
+
+import random
+import hashlib
+
+
+def chaineAlea(longueur):
+    caracteres = "AZERTYYUIOPQSDFGHJKLMWXCVBN" #dans l'ordre du clavier
+    mot = "" #Variable mot 
+    compteur = 0 #Compteur de lettres
+    while compteur < longueur:
+        lettre = caracteres[random.randint(0, len(caracteres)-1)] #On tire au hasard une lettre
+        mot += lettre #On  ajoute la lettre au mot 
+        compteur += 1 #On incrémente le compteur de lettres
+    data = ""
+    data = mot.encode()
+    hash_object = hashlib.sha256(data)
+    HashmotHexa = hash_object.hexdigest() 
+#    print ("HashmotHexa : ",  HashmotHexa)
+    return HashmotHexa
+
+
+def rechercheDebut(Debut, longueur) :
+    chaine = chaineAlea(longueur)
+    print("chaine : ",  chaine)
+    if chaine.find(Debut) ==-1: #tant que le Debut n'est pas dans la chaine 
+        print("pas de présence de Debut dans la chaine ",  chaine)
+        rechercheDebut(Debut, longueur)  
+    else :
+            if chaine.find(Debut) !=0 : #si le Debut n'est pas au début de la chaine
+                print("Debut est dans la chaine mais pas au début .. ",  chaine)
+                rechercheDebut(Debut, longueur)
+            else :
+                print("La chaîne trouvée est : ",  chaine )
+
+
+    
+
+if __name__ == "__main__":
+    print ("modèle de pow naîve : longueur du mot <= 8 - Debut du mot : [nx ex: 7c] ")
+    longueur = int(input('Entrez une longueur [8 maxi] : '))
+    Debut = str(input("Entrez un début de chaîne Hexa [2 caractères maxi] : ")) 
+    Debut = Debut.lower()
+    rechercheDebut( Debut, longueur )
+    
+#python -m cProfile fact.py en ligne de commande
+#Le rapport fournit présente plusieurs champs intéressants :
+#ncalls : le nombre d’appels de la fonction
+#tottime : founit le temps total en secondes passé dans la fonction en excluant les sous-fonctions
+#percall : est égal à tottime/ncalls
+#cumtime : est le temps passé dans la fonction et les sous-fonctions1
+#filename :lineno(function) : donne l’information sur la fonction testée ainsi que sa position (fichier + numéro de ligne)
